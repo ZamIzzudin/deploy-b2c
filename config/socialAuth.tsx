a69f2a4952794ae658e9907933e4e0d109fcdb87
ZamIzzudin <azzamizzudinhasan@gmail.com>
2022-07-07 21:40:14 +0700

    change firebase from development phase

--- config/socialAuth.tsx
+++ config/socialAuth.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/extensions */
 /* eslint-disable import/prefer-default-export */
-import firebase from './firebase-config';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 export const facebookProvider = new firebase.auth.FacebookAuthProvider();
 export const googleProvider = new firebase.auth.GoogleAuthProvider();
