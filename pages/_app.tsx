92a56532ef96d3c4e083643c20c70e1c49656ce9
ZamIzzudin <azzamizzudinhasan@gmail.com>
2022-09-07 21:05:09 +0700

    delete meta tag for http

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,7 +33,6 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <link rel="icon" href="/favicon2.png" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossOrigin="anonymous" />
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
       </Head>
       <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossOrigin="anonymous" />
       <Script src="https://kit.fontawesome.com/63c3db6def.js" crossOrigin="anonymous" />
